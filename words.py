0db0891cfa039c55187b494d34010b942fa4e096
unknown <phamm@carleton.edu>
2022-04-19 14:33:03 -0500

    words day 3 uploaded

--- words.py
+++ words.py
@@ -159,7 +159,54 @@ examplesDay2 = ["The least a government can do is to decry human rights abuses",
 			    "He is an affable and agreeable companion. I would love to hangout with him more.",\
 			    "The cliffs have been eroded by the sea. The erosion is unstoppable."]
 
-everything = [[wordsDay0, definitionsDay0, examplesDay0], [wordsDay1, definitionsDay1, examplesDay1], [wordsDay2, definitionsDay2, examplesDay2]]
+wordsDay3 = ["chivalrous", "impetuous", "boorish", "circumspect", "forbear",\
+			 "jettison", "contrived", "adversarial", "untenable", "efficacious",\
+			 "dichotomy", "adduce", "trifling", "conspicuous", "precarious",\
+			 "incommensurable", "acrimony", "dissension", "comity", "mordant"]
+definitionsDay3 = ["courteous and gallant, especially toward women",\
+				   "acting or done quickly and without thought or care",\
+				   "rough and bad-mannered",\
+				   "risk-averse, unwilling to take risks",\
+				   "politely or patiently restrain an impulse to do something; refrain",\
+				   "abandon or discard",\
+				   "deliberately created rather than arising naturally or spontaneously",\
+				   "involving or characterized by conflict or opposition",\
+				   "(especially of a postion or view) not able to be maintained or defended against attack or objection",\
+				   "successful in producing a desired or intended result; effective",\
+				   "a division or contrast between two things that are or are represented as being opposed or entirely different"
+				   "cite as evidence",\
+				   "unimportant or trivial",\
+				   "standing out so as to be clearly visible",\
+				   "not securely held or in position; dangerously likely to fall or collapse, or uncertain, dependent on chance",\
+				   "not able to be judged by the same standard as something",\
+				   "bitterness or ill feeling",\
+				   "disagreement that leads to discord",\
+				   "courtesy and considerate behavior toward others",\
+				   "(a thought) having or showing a sharp or critical quality; biting"]
+examplesDay3 = ["Shall I be chivalrous and offer you my coat.",\
+			    "Her friend was headstrong and impetuous as he never thinks twice.",\
+			    "The irascible teach will not condone any boorish behaviors.",\
+			    "A circumspect trader would not buy Auzuki after the pump; it's not a safe play.",\
+			    "He is so modest that he even decided to forbear to include his own work.",\
+			    "Individuals are often forced to jettison certain attitudes and behaviors.",\
+			    "The ending of the novel is too contrived and too forced.",\
+			    "Miller reminded his clients that labor relationship are inherently adversarial as the interests of business owners are diametrically opposed to those of employees."
+			    "This argument is clearly untenable and thus should be dropped.",\
+			    "The vaccine has proved both efficacious and safe",\
+			    "Although progressive and reactionary populist movements seem like dichotomy they actually share certain features".
+			    "The researcher hopes to adduce at least 5 results to explain the situation.",\
+			    "Although the introduction of the new fish theorectially would have detrimental effects on other fish, it turns out be pretty trifling and trivial.",\
+			    "The contribution he has shown was conspicuous although he doesn't have to speak much.",\
+			    "He made a precarious living as a painter.",\
+			    "Systems of belief prevalent during different periods in history are incommensurable and therefore cannot be understood except in their own term.",\
+			    "The debate has turned acrimony and finger-pointing. Time to end this.",\
+			    "These issues caused bitter dissension in the party",\
+			    "The company suffers from an almost total lack of comity as even the most innocuouscommunications between departments lend to devolve into acrimony",\
+			    "He joined the Extreme Left, and his energy and mordant eloquence speedily made him the leader of the Radical section"]
+
+
+everything = [[wordsDay0, definitionsDay0, examplesDay0], [wordsDay1, definitionsDay1, examplesDay1], [wordsDay2, definitionsDay2, examplesDay2],\
+			  [wordsDay3, definitionsDay3, examplesDay3]]
 
 allWords = []
 allDefinitions = []
